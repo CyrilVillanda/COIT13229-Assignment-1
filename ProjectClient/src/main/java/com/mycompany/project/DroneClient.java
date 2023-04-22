5a1d797b0fcfaa39df99a72962576e081e967187
CyrilVillanda <129810554+CyrilVillanda@users.noreply.github.com>
2023-04-23 00:22:10 +1000

    Implemented connection
    Code taken from previous projects

--- ProjectClient/src/main/java/com/mycompany/project/DroneClient.java
+++ ProjectClient/src/main/java/com/mycompany/project/DroneClient.java
@@ -5,10 +5,46 @@
  */
 package com.mycompany.project;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author cjvil
  */
 public class DroneClient {
+    public static void main (String args[]) {
+     //arguments supply message and hostname of destination
+     //if running from a command prompt
+    Socket s=null;
+    String hostName = "localhost";
+    String message ="Output works";
+    try{
+    int serverPort=7896;
     
+    s=new Socket(hostName, serverPort);    
+    DataInputStream in=new DataInputStream(s.getInputStream());
+    DataOutputStream out=new DataOutputStream(s.getOutputStream());
+    out.writeUTF(message);
+        String data = in.readUTF();       
+    System.out.println("Message Received From Server: "+ data) ;      
+       } catch (UnknownHostException e){
+       System.out.println("Sock:"+e.getMessage()); 
+    } catch (EOFException e){
+       System.out.println("EOF:"+e.getMessage());
+        } catch (IOException e){
+       System.out.println("IO:"+e.getMessage());
+        }
+    finally {
+       if(s!=null)
+         try {
+              s.close();
+             } catch (IOException e){
+        System.out.println("close:"+e.getMessage());}
+        } 
+    }
 }
